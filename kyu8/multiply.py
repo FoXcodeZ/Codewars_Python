41f26bf0e12cbde6e06a3d844cd2035e4d51b705
FoXcodeZ <m.grabowy@int.pl>
2022-11-09 00:21:06 +0100

    added multiply()

---
+++ kyu8/multiply.py
@@ -0,0 +1,8 @@
+def multiply(t_numberA, t_numberB):
+    return t_numberA * t_numberB
+
+
+if __name__ == '__main__':
+    numberA = 4
+    numberB = 12
+    print(f"{numberA} * {numberB} == {multiply(numberA, numberB)}")
